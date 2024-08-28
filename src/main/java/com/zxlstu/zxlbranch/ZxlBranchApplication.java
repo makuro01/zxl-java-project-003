f83d0a8cccda8c64552f5ccd37940bb263d4f4a3
zxl <xiaoarashi@gmail.com>
2024-08-29 08:49:36 +0900

    fist branch

---
+++ src/main/java/com/zxlstu/zxlbranch/ZxlBranchApplication.java
@@ -0,0 +1,11 @@
+package com.zxlstu.zxlbranch;
+
+/**
+ * @author 張　曉竜
+ * @java se version 1.0
+ **/
+public class ZxlBranchApplication {
+    public static void main(String[] args) {
+        System.out.println("hello branch");
+    }
+}
