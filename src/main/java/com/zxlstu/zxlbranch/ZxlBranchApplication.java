543095c1194d3ad6e94d19fe547b9776748e557c
zxl <xiaoarashi@gmail.com>
2024-08-29 12:43:49 +0900

    无冲突

--- src/main/java/com/zxlstu/zxlbranch/ZxlBranchApplication.java
+++ src/main/java/com/zxlstu/zxlbranch/ZxlBranchApplication.java
@@ -7,5 +7,6 @@ package com.zxlstu.zxlbranch;
 public class ZxlBranchApplication {
     public static void main(String[] args) {
         System.out.println("hello branch");
+        System.out.println("hello branch123");
     }
 }
